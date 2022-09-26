edff595fdbbda009af4dd2f1f3f04f0c5e775b03
Amit kohar <amitkohar@Amits-MacBook-Air.local>
2022-09-25 20:42:40 -0400

    added contact list

--- script/app.js
+++ script/app.js
@@ -1,8 +1,42 @@
+"use strict";
+
+
 (function()
 {
- function start()
- (
-     console.log("App Started")
- )
- window.addEventListener("load",start);
+    
+    function LoadData(method,url,callback){
+        let XHR = new XMLHttpRequest();
+        XHR.open(method,url);
+        XHR.send();
+        XHR.addEventListener("readystagechange",function(){
+            if((XHR.status == 200) && (XHR.readystate == 4))
+            {
+                // console.log(XHR.responseText);
+                // let contactDataSource=JSON.parse(XHR.responseText);
+                // console.log(contactDataSource.ContactList[0])
+                callback(XHR.responseText)
+            }
+        });
+    }
+    // 1st way of using function
+    function start()
+    {
+        console.log("App Started")
+        LoadData("GET","./Data/contacts.json",function(XHR){
+            console.log(XHR);
+        });
+    }
+ 
+    window.addEventListener("load",start);
 })();
+
+
+
+// (function()
+// {
+//  function start()
+//  {
+//      console.log("App Started")
+//  }
+//  window.addEventListener("load",start);
+// })();
